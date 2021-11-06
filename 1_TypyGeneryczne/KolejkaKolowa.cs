bb4245c14ca070d8547232b306fd2d0617f015e6
Jurek <jer.adamski@gmail.com>
2021-11-06 19:11:56 +0100

    1_3 NaszaKolekcja

---
+++ 1_TypyGeneryczne/KolejkaKolowa.cs
@@ -0,0 +1,63 @@
+﻿namespace _1_TypyGeneryczne
+{
+    public class KolejkaKolowa
+    {
+        private double[] bufor;
+        private int poczatekBufora;
+        private int koniecBufora;
+
+        //konstruktor domyślny. ctor
+        public KolejkaKolowa() : this(pojemnosc: 5) //to znaczy, że jeżeli nie zostanie wskazana pojemność to przyjmie 5
+        {
+                
+        }
+
+        public KolejkaKolowa(int pojemnosc)
+        {
+            bufor = new double[pojemnosc + 1];
+            poczatekBufora = 0;
+            koniecBufora = 0;
+
+        }
+
+        public void Zapisz (double wartosc)
+        {
+            bufor[koniecBufora] = wartosc;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+
+            if (koniecBufora == poczatekBufora)
+                poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+        }
+
+        public double Czytaj ()
+        {
+            var wynik = bufor[poczatekBufora];
+            poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+
+        public int Pojemnosc
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+
+        public bool JestPusty
+        {
+            get
+            {
+                return koniecBufora == poczatekBufora;
+            }
+        }
+
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == poczatekBufora;
+            }
+        }
+    }
+}
